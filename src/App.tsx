ca4f7a7c556e9839cd638923a8b4db4955472431
José Antonio <josejr017@gmail.com>
2025-05-14 13:55:32 -0300

    changing theme

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,8 @@ export function App() {
               type="text"
               id="myInput"
               placeholder="Digite algo"
-              className="text-center p-2 border-transparent border-b-2 rounded-none border-b-red-400 transition-all duration-100 ease-in-out 
-              placeholder:italic focus:rounded-lg disabled:border-b-gray-300 disabled:text-gray-400 min-w-96" />
+              className="text-center p-2 mx-20 border-transparent border-b-2 rounded-none border-b-red-400 transition-all duration-100 ease-in-out 
+              placeholder:italic focus:rounded-lg disabled:border-b-gray-300 disabled:text-gray-400 " />
           </div>
 
           <div className="flex flex-col items-center justify-center gap-6">
@@ -43,7 +43,7 @@ export function App() {
           </div>
 
           <div className="flex flex-col items-center justify-center gap-6">
-            <Button className="min-w-96 bg-chart-2 border-none p-4 rounded-xl my-12 mx-0 cursor-pointer hover:bg-chart-2/80 transition-all duration-100 ease-in-out" >
+            <Button className="w-full mx-0  bg-chart-2 border-none p-4 rounded-xl my-12 cursor-pointer hover:bg-chart-2/80 transition-all duration-100 ease-in-out" >
               <PlayCircleIcon className="min-w-8 min-h-8"/>
             </Button>
             <Button className="min-w-96 bg-ring border-none p-4 rounded-xl my-12 mx-0 cursor-pointer hover:bg-ring/80 transition-all duration-100 ease-in-out" >
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,59 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+    const [theme, setTheme] = useState<AvailableThemes>(
+        () => {
+            const storedTheme = localStorage.getItem('theme') as AvailableThemes ;
+            return storedTheme || 'dark';
+        }
+    );
+
+    function handleThemeChange(event: React.MouseEvent) {
+        event.preventDefault();
+        
+        setTheme(prevTheme => {
+            const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+            return nextTheme
+        })
+    }
+
+    useEffect(() => {
+    document.documentElement.classList.remove('light', 'dark');
+    document.documentElement.classList.add(theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
     return (
-        <nav className="flex items-center justify-center gap-6 text-gray-900">
-            <a href="#" className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400  hover:bg-red-400/80 transition duration-150 ease-in-out">
-                <HouseIcon size={24} />    
+        <nav className="flex items-center justify-center gap-6 text-white-900">
+            <a
+                href="#" aria-label="Ir para a página inicial" title="Ir para a página inicial"
+                className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400  hover:bg-red-400/80 transition duration-150 ease-in-out">
+                <HouseIcon size={24} />
             </a>
 
-            <a href="#" className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400 hover:bg-red-400/80 transition duration-150 ease-in-out">
-                <HistoryIcon size={24} />    
-            </a>
-            
-            <a href="#" className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400 hover:bg-red-400/80 transition duration-150 ease-in-out">
-                <SettingsIcon size={24} />  
+            <a
+                href="#" aria-label="Ver histórico" title="Ver histórico"
+                className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400 hover:bg-red-400/80 transition duration-150 ease-in-out">
+                <HistoryIcon size={24} />
             </a>
 
-            <a href="#" className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400 hover:bg-red-400/80 transition duration-150 ease-in-out">
-                <SunIcon size={24} />
+            <a
+                href="#" aria-label="Configurações" title="Configurações"
+                className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400 hover:bg-red-400/80 transition duration-150 ease-in-out">
+                <SettingsIcon size={24} />
+            </a>
+
+            <a
+                href="#" aria-label="Mudar tema" title="Mudar tema"
+                className="inline-flex items-center justify-center p-5 border rounded-md border-red-400 bg-red-400 hover:bg-red-400/80 transition duration-150 ease-in-out"
+                onClick={ handleThemeChange }
+                >
+                
+                {theme === 'dark' ? <SunIcon size={24} /> : <MoonIcon size={24} />}
+                
             </a>
         </nav>
     );
