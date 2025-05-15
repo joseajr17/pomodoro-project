f9e45fc4bb0419cb2a2096d5488f8258f4302bc5
José Antonio <josejr017@gmail.com>
2025-05-15 19:08:07 -0300

    Cycles iteration and buttons updates

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,24 +1,41 @@
+import { useTaskContext } from "@/contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "@/utils/getNextCycle";
+import { getNextCycleType } from "@/utils/getNextCycleType";
+
 export function Cycles() {
+    const { state } = useTaskContext();
+
+    const cycleStage = Array.from({ length: state.currentCycle });
+
+    const bgSpan = {
+        longBreakTime: "bg-chart-1",
+        shortBreakTime: "bg-red-600",
+        workTime: "bg-chart-2",
+    };
+
+    const cycleTypeDescription = {
+        longBreakTime: "descanso longo",
+        shortBreakTime: "descanso curto",
+        workTime: "foco",
+    };
+
     return (
         <div className="flex flex-col gap-7 items-center justify-center">
             <span>Ciclos:</span>
             <div className="flex gap-3">
-                {/* Work Time */}
-                <span className="w-8 h-8 rounded-2xl bg-red-600"></span>
-                {/* Short Break Time */}
-                <span className="w-8 h-8 rounded-2xl bg-chart-4"></span>
-                {/* Work Time */}
-                <span className="w-8 h-8 rounded-2xl bg-red-600"></span>
-                {/* Short Break Time */}
-                <span className="w-8 h-8 rounded-2xl bg-chart-4"></span>
-                {/* Work Time */}
-                <span className="w-8 h-8 rounded-2xl bg-red-600"></span>
-                {/* Short Break Time */}
-                <span className="w-8 h-8 rounded-2xl bg-chart-4"></span>
-                {/* Work Time */}
-                <span className="w-8 h-8 rounded-2xl bg-red-600"></span>
-                {/* Long Break Time */}
-                <span className="w-8 h-8 rounded-2xl bg-chart-1"></span>
+                {cycleStage.map((_, index) => {
+                    const nextCycle = getNextCycle(index);
+                    const cycleType = getNextCycleType(nextCycle);
+
+                    return (
+                        <span
+                            key={nextCycle}
+                            className={`w-8 h-8 rounded-2xl ${bgSpan[cycleType]}`}
+                            aria-label={`Ciclo de ${cycleTypeDescription[cycleType]}`}
+                            title={`Ciclo de ${cycleTypeDescription[cycleType]}`}
+                        ></span>
+                    );
+                })}
             </div>
         </div>
     );
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -49,7 +49,18 @@ export function Form() {
                 formattedSecondsRemaining: secondsToMinutes(secondsRemaining),
                 activeTask: newTask,
                 currentCycle: nextCycle, // 1 a 8
-                config: {...prevState.config,}
+                config: { ...prevState.config, }
+            }
+        });
+    }
+
+    function handleInterruptTask() {
+        setState(prevState => {
+            return {
+                ...prevState,
+                secondsRemaining: 0,
+                formattedSecondsRemaining: '00:00',
+                activeTask: null,
             }
         });
     }
@@ -67,24 +78,45 @@ export function Form() {
                     placeholder="Digite algo"
                     ref={taskNameInput}
                     className="text-center p-2 mx-20 border-transparent border-b-2 rounded-none border-b-red-400 transition-all duration-100 ease-in-out 
-              placeholder:italic focus:rounded-lg disabled:border-b-gray-300 disabled:text-gray-400 " />
+              placeholder:italic focus:rounded-lg disabled:border-b-gray-300 disabled:text-gray-400 "
+                    disabled={!!state.activeTask}
+                />
             </div>
 
             <div className="flex flex-col items-center justify-center gap-6">
                 <p>Lorem ipsum dolor sit amet.</p>
             </div>
 
-            <div className="flex flex-col items-center justify-center gap-6">
-                <Cycles />
-            </div>
+            {state.currentCycle > 0 && (
+                <div className="flex flex-col items-center justify-center gap-6">
+                    <Cycles />
+                </div>
+            )}
 
-            <div className="flex flex-col items-center justify-center gap-6">
-                <Button className="w-full mx-0  bg-chart-2 border-none p-4 rounded-xl my-12 cursor-pointer hover:bg-chart-2/80 transition-all duration-100 ease-in-out" >
-                    <PlayCircleIcon className="min-w-8 min-h-8" />
+            <div className="flex flex-row items-center justify-center gap-6">
+                <Button
+                    type="submit"
+                    aria-label="Iniciar nova tarefa"
+                    title="Iniciar nova tarefa"
+                    className="min-w-1/2 h-auto mx-0 bg-black dark:bg-white dark:text-black  border-none rounded-xl my-12 cursor-pointer hover:bg-black/80 dark:hover:bg-white/80 transition-all duration-100 ease-in-out"
+                    key="btn_submit-task"
+                    disabled={!!state.activeTask}
+                >
+                    <PlayCircleIcon className="min-w-10 min-h-10" /> Start
                 </Button>
-                <Button className="min-w-96 bg-ring border-none p-4 rounded-xl my-12 mx-0 cursor-pointer hover:bg-ring/80 transition-all duration-100 ease-in-out" >
-                    <StopCircleIcon className="min-w-8 min-h-8" />
+
+                <Button
+                    type="button"
+                    aria-label="Interromper tarefa atual"
+                    title="Interromper tarefa atual"
+                    className="min-w-1/2 h-auto bg-ring border-none rounded-xl my-12 mx-0 cursor-pointer hover:bg-ring/80 transition-all duration-100 ease-in-out"
+                    onClick={handleInterruptTask}
+                    key="btn_interrupt-task"
+                    disabled={!state.activeTask}
+                >
+                    <StopCircleIcon className="min-w-10 min-h-10" /> Stop
                 </Button>
+
             </div>
         </form>
     );
