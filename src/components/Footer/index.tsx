d2b5691a337e5649497a9839ed9e7ec69069dd37
José Antonio <josejr017@gmail.com>
2025-05-19 11:26:26 -0300

    update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export function Footer() {
     return (
-        <footer className="bg-gray-900 text-white py-6 mt-10">
+        <footer className="bg-transparent text-white py-6 mt-10">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center space-y-4">
                 <p className="text-sm text-gray-400">© 2025 José Antonio.</p>
 
@@ -12,7 +12,7 @@ export function Footer() {
                         href="https://github.com/joseajr17"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-gray-400 hover:text-white transition"
+                        className="text-gray-400 hover:text-black dark:hover:text-white transition"
                     >
                         <FaGithub size={24} />
                     </a>
@@ -22,7 +22,7 @@ export function Footer() {
                         href="https://www.linkedin.com/in/joseantonio1712/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-gray-400 hover:text-white transition"
+                        className="text-gray-400 hover:text-black dark:hover:text-white transition"
                     >
                         <FaLinkedin size={24} />
                     </a>
@@ -32,7 +32,7 @@ export function Footer() {
                         href="https://mail.google.com/mail/?view=cm&fs=1&to=josejr017@gmail.com"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-gray-400 hover:text-white transition"
+                        className="text-gray-400 hover:text-black dark:hover:text-white transition"
                     >
                         <FaEnvelope size={24} />
                     </a>
